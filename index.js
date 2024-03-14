f36ba0cef5ed80dcefda4e95ab417e521c58100c
Suman <102908770+LRSSuman@users.noreply.github.com>
2024-03-14 08:42:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const toggleSidebar = document.getElementById('toggleSidebar');
 const sidebar = document.getElementById('sidebar');
 const blurScreen = document.getElementById('blurScreen');
 const closeSidebar = document.getElementById('closeSidebar');
+const body = document.getElementById('body');
 
 toggleSidebar.addEventListener('click', () => {
     sidebar.classList.toggle('sidebar-open');
     blurScreen.classList.toggle('blur-active');
+    body.classList.toggle('no-scroll');
 });
 
 closeSidebar.addEventListener('click', () => {
     sidebar.classList.remove('sidebar-open');
     blurScreen.classList.remove('blur-active');
+    body.classList.remove('no-scroll');
 });
 
 blurScreen.addEventListener('click', () => {
     sidebar.classList.remove('sidebar-open');
     blurScreen.classList.remove('blur-active');
+    body.classList.remove('no-scroll');
 });
